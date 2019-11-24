1430e55c084767da567ff8f0fa5d08f1c89b55a3
Tayyab Razzaq <tayyab.razzaq401@gmail.com>
2019-11-24 12:40:36 +0500

    feat: city model created and added relation with artist and venue

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from flask import Flask
 from flask_moment import Moment
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from constants import StatesEnum
 
 # ==================================================================================================================== #
 # App Config.
@@ -23,17 +23,48 @@ migrate = Migrate(app, db)
 # Models.
 # ==================================================================================================================== #
 
-class Venue(db.Model):
+class BaseModel(db.Model):
+    """Mixin / Abstract for model to add easy create and delete functionality."""
+    __abstract__ = True
+
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    modified_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class City(BaseModel):
+    """City Table."""
+    __tablename__ = 'City'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    state = db.Column(db.Enum(StatesEnum))
+    venues = db.relationship('Venue', backref='city')
+    artists = db.relationship('Artist', backref='city')
+
+
+class Venue(BaseModel):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    city_id = db.Column(db.Integer, db.ForeignKey('City.id'))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+
+    @property
+    def state(self):
+        """Get State."""
+        return self.city.state
 
     def __repr__(self):
         """
@@ -46,17 +77,21 @@ class Venue(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 
-class Artist(db.Model):
+class Artist(BaseModel):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    city_id = db.Column(db.Integer, db.ForeignKey('City.id'))
+
+    @property
+    def state(self):
+        """Get State."""
+        return self.city.state
 
     def __repr__(self):
         """
