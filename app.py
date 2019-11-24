4a4a0a963ced730b49d76242ddad5a1157377a18
Tayyab Razzaq <tayyab.razzaq401@gmail.com>
2019-11-24 10:51:53 +0500

    refactor: connected to local database

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
@@ -24,12 +25,11 @@ from forms import *
 # ----------------------------------------------------------------------------#
 
 app = Flask(__name__)
-moment = Moment(app)
 app.config.from_object('config')
+moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-# TODO: connect to a local postgresql database
 
 # ----------------------------------------------------------------------------#
 # Models.
